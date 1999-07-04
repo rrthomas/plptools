2b9c8881877dcb1835f17a0c5b642784c738049f
Fritz Elfert <felfert@to.com>
1999-07-04 12:00:07 +0000

    Initial import.

---
+++ lib/log.cc
@@ -0,0 +1,25 @@
+//#include <iostream.h>
+//#include <iomanip.h>
+
+#include "log.h"
+
+logbuf::logbuf(int _level) {
+	ptr = buf;
+	len = 0;
+	level = _level;
+}
+
+int logbuf::overflow(int c) {
+	if (c == '\n') {
+		*ptr++ = '\n';
+		*ptr = '\0';
+		syslog(level, buf);
+		ptr = buf;
+		return 0;
+	}
+	if ((len + 2) >= sizeof(buf))
+		return EOF;
+	*ptr++ = c;
+	len++;
+	return 0;
+}
---
+++ lib/log.h
@@ -0,0 +1,18 @@
+#ifndef _log_h_
+#define _log_h_
+
+#include <ostream.h>
+#include <syslog.h>
+
+class logbuf : public streambuf {
+	public:
+		logbuf(int);
+		int overflow(int c = EOF);
+	private:
+		char *ptr;
+		int len;
+		int level;
+		char buf[1024];
+};
+
+#endif
