ee5d8c447c36d1798ae2c125a95ead663501d3f3
Fritz Elfert <felfert@to.com>
2000-07-31 22:53:27 +0000

    Removed more intermediate files from CVS. Those can be rebuilt by using Makefile.cvs. Of course, a developer should all of the necessary tools installed (automake, autoconf, libtool).

--- acconfig.h
+++ acconfig.h
@@ -1,3 +1,9 @@
+/* Define this, if you have libreadline */
+#undef HAVE_LIBREADLINE
+
+/* Define this, if you have libhistory */
+#undef HAVE_LIBHISTORY
+
 /* Define if the C++ compiler supports BOOL */
 #undef HAVE_BOOL
 
