2a67b68ec993c1c0289ae20594e64e7839291c42
Reuben Thomas <rrt@sc3d.org>
2007-12-08 00:52:08 +0000

    Since configure.in.in is not processed, rename it to configure.ac.
    Simplify Makefile.cvs into pure setup and rename autogen.sh.

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+# Set up for compilation
+
+cp Makefile.am.in Makefile.am
+mkdir -p intl
+echo n | gettextize --force --intl --no-changelog
+test -e po/Makevars || cp po/Makevars.template po/Makevars
+rm -f ChangeLog *~
+-test -f po/ChangeLog~ && mv -f po/ChangeLog~ po/ChangeLog
+autoreconf -i
