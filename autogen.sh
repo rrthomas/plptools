ff5ad5642896cc0ec7329d283c3fc0bd3f9670d4
Reuben Thomas <rrt@sc3d.org>
2007-12-09 15:45:18 +0000

    Don't remove ChangeLog

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,6 @@ cp Makefile.am.in Makefile.am
 mkdir -p intl
 echo n | gettextize --force --intl --no-changelog
 test -e po/Makevars || cp po/Makevars.template po/Makevars
-rm -f ChangeLog *~
+rm -f *~
 -test -f po/ChangeLog~ && mv -f po/ChangeLog~ po/ChangeLog
 autoreconf -i
