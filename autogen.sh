600afc7ff1df3aedc69dd34d7a3d8c0a40b10a82
Reuben Thomas <rrt@sc3d.org>
2007-12-09 16:09:18 +0000

    Remove autogen.sh and hence Makefile.am.in (becomes Makefile.am), and add a note about use of autoreconf to INSTALL.

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-# Set up for compilation
-
-cp Makefile.am.in Makefile.am
-mkdir -p intl
-echo n | gettextize --force --intl --no-changelog
-test -e po/Makevars || cp po/Makevars.template po/Makevars
-rm -f *~
--test -f po/ChangeLog~ && mv -f po/ChangeLog~ po/ChangeLog
-autoreconf -i
