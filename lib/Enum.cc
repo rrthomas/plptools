de775e0d38485545801c23f23af9134d76980911
Reuben Thomas <rrt@sc3d.org>
2008-03-13 17:04:06 +0000

    Remove unneeded FIXME and fix some English.

--- lib/Enum.cc
+++ lib/Enum.cc
@@ -55,18 +55,18 @@ string EnumBase::i2sMapper::lookup (long i) const {
 
 long EnumBase::i2sMapper::lookup (const char *s) const {
     /*
-    * lookup a specific string.
+    * look up a specific string.
     * Since speed does not matter, we just do an exhaustive
     * search.
     * Otherwise we would have to maintain another map
-    * mapping strings to ints .. but its not worth the memory
+    * mapping strings to ints .. but it's not worth the memory
     */
     i2s_map_t::const_iterator run = stringMap.begin();
     while (run != stringMap.end() && strcmp(s, run->second)) {
 	++run;
     }
     if (run == stringMap.end())
-	return  -1;    // FIXME .. maybe throw an exception ?
+	return  -1;
     return run->first;
 }
 
