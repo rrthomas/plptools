0c3e189448b77d17302ed3a9dd66f08c064bb341
Reuben Thomas <rrt@sc3d.org>
2024-02-24 10:59:27 +0000

    Add missing copyright headers to files in etc/ (fix #9)

--- etc/ttytap.c
+++ etc/ttytap.c
@@ -1,3 +1,23 @@
+/* plpfuse/Makefile.am
+ *
+ * This file is part of plptools.
+ *
+ * Copyright (C) 2001 Fritz Elfert <felfert@to.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/fcntl.h>
