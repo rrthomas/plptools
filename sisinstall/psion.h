9a1fef989d53d2328422055f4b05e82d593cf946
Reuben Thomas <rrt@sc3d.org>
2024-02-27 06:36:24 +0000

    sisinstall: add missing copyright headers

--- sisinstall/psion.h
+++ sisinstall/psion.h
@@ -1,3 +1,23 @@
+/*
+ * This file is part of plptools.
+ *
+ *  Copyright (C) 2002 Daniel Brahneborg <basic@chello.se>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  along with this program; if not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef _PSION_H
 #define _PSION_H
 
