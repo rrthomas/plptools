9a4e5629c6f49710386359d84c914ecb732c0d1e
Reuben Thomas <rrt@sc3d.org>
2024-07-04 13:50:07 +0300

    build.sh: add debug for macOS

--- build-aux/build.sh
+++ build-aux/build.sh
@@ -9,6 +9,9 @@ set -e
 if [[ "$ASAN" == "yes" ]]; then
     CONFIGURE_ARGS+=(CFLAGS="-g3 -fsanitize=address -fsanitize=undefined" CXXFLAGS="-g3 -fsanitize=address -fsanitize=undefined" LDFLAGS="-fsanitize=address -fsanitize=undefined")
 fi
+# Debugging
+echo LDFLAGS: $LDFLAGS
+echo CPPFLAGS: $CPPFLAGS
 ./configure "${CONFIGURE_ARGS[@]}"
 make V=1
 make distcheck || ( cat ./plptools-*/_build/sub/tests/test-suite.log; exit 1 )
